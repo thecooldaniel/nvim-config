5acbcfacf3145048b35a9134cac6daf4c8a4cb8d
Daniel Owens <kyoji1@gmail.com>
2024-05-24 23:10:31 -0400

    Initial push

---
+++ lua/plugins/language/systemverilog.lua
@@ -0,0 +1,43 @@
+local systemverilog = {}
+
+function systemverilog.setupLsp()
+    require('lspconfig').verible.setup({
+      -- cmd = { 'verible-verilog-ls', '--rules_config_search' },
+      cmd = { 'verible-verilog-ls' },
+      root_dir = require('lspconfig').util.root_pattern({'.git', 'verilator.f'}),
+      -- capabilities = capabilities,
+      format_on_save = true,
+    })
+end
+
+function systemverilog.setupLinter(lint)
+  
+  lint.linters_by_ft = {
+    systemverilog = { 'verilator' },
+    verilog = { 'verilator' },
+  }
+
+  local verilator = lint.linters.verilator
+  verilator.args = {
+    "-sv",
+    "-Wall",
+    "--bbox-sys",
+    "--bbox-unsup",
+    "--lint-only",
+    "-Irtl/include",
+    '-f',
+    vim.fs.find('verilator.f', {upward = true, stop = '/home'})[1],
+
+
+  lint.linters.verilator = verilator
+  -- lint.linters["verilator"] = verilator
+
+end
+
+function systemverilog.setupTreesitter(opts)
+  table.insert(opts.ensure_installed, 'verilog')
+  table.insert(opts.highlight.disable, {'verilog', 'systemverilog'})
+end
+
+
+return systemverilog
