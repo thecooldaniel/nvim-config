8a905e24422c6d8aae2db796a5bb39d88a7963df
daniel <daniel@daniel-owens.com>
2024-06-01 15:06:52 -0400

    Change 'T' namespace to 'Tabs'

--- lua/plugins/navigation/init.lua
+++ lua/plugins/navigation/init.lua
@@ -16,7 +16,7 @@ return {
         ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
         ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
         ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
+        ['<leader>t'] = { name = '[T]abs', _ = 'which_key_ignore' },
         ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
         ['<leader>l'] = { name = '[L]anguage', _ = 'which_key_ignore'},
       }
