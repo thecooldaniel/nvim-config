26b0c2871453084ffd3e853b08cd4b4afd3a2644
Daniel Owens <kyoji1@gmail.com>
2024-06-02 21:56:24 -0400

    Remove calls to setup() for unused colorschemes

--- lua/plugins/colorschemes/init.lua
+++ lua/plugins/colorschemes/init.lua
@@ -2,11 +2,11 @@ return {
   {
     "loctvl842/monokai-pro.nvim",
     config = function()
-      require("monokai-pro").setup({
+      local setup = {
         terminal_background = true,
         filter = "octagon"
-      })
-
+      }
+      -- require("monokai-pro").setup(setup)
       -- vim.cmd 'colorscheme monokai-pro'
     end
   },
@@ -16,34 +16,35 @@ return {
     -- priority = 1000,
     opts = {},
     config = function()
-      require('solarized-osaka').setup({
+      local setup = {
         terminal_colors = true,
-      })
+      }
+      -- require('solarized-osaka').setup(setup)
       -- vim.cmd 'colorscheme solarized-osaka'
     end
   },
   {
     "oxfist/night-owl.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
+    -- lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    -- priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- load the colorscheme here
-      require("night-owl").setup()
+      -- require("night-owl").setup()
       -- vim.cmd.colorscheme("night-owl")
     end,
   },
   {
     'rebelot/kanagawa.nvim',
     config = function()
-      require('kanagawa').setup()
+      -- require('kanagawa').setup()
       -- vim.cmd.colorscheme('kanagawa-wave')
     end,
   },
   {
     "bluz71/vim-nightfly-colors",
     name = "nightfly",
-    lazy = false,
-    priority = 1000,
+    -- lazy = false,
+    -- priority = 1000,
     config = function()
       vim.g.nightflyCursorColor = true
       vim.g.nightflyNormalFloat = true
@@ -51,8 +52,14 @@ return {
       vim.g.nightflyUnderlineMatchParen = true
       vim.g.nightflyWinSeparator = 2
       vim.opt.fillchars = { horiz = '━', horizup = '┻', horizdown = '┳', vert = '┃', vertleft = '┫', vertright = '┣', verthoriz = '╋', }
-
-      vim.cmd.colorscheme('nightfly')
     end
   },
+  {
+    "folke/tokyonight.nvim",
+    -- lazy = false,
+    -- priority = 1000,
+    opts = {
+      style = 'night'
+    },
+  }
 }
