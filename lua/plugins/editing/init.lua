86478e4c3149557f3ddb6f8a4b2c17689aeae6f9
Daniel Owens <kyoji1@gmail.com>
2024-06-08 22:10:33 -0400

    Disable UFO for now

--- lua/plugins/editing/init.lua
+++ lua/plugins/editing/init.lua
@@ -45,21 +45,21 @@ return {
       -- configurations go here
     },
   },
-  {
-    'kevinhwang91/nvim-ufo', 
-    dependencies = 'kevinhwang91/promise-async',
-    init = function()
-        vim.o.foldcolumn = '1' -- '0' is not bad
-        vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-        vim.o.foldlevelstart = 99
-        vim.o.foldenable = true
-    end,
-    config = function()
-      require'ufo'.setup{
-        provider_selector = function(bufnr, filetype, buftype)
-          return {'treesitter', 'indent'}
-        end
-      }
-    end
-  }
+  -- {
+  --   'kevinhwang91/nvim-ufo', 
+  --   dependencies = 'kevinhwang91/promise-async',
+  --   init = function()
+  --       vim.o.foldcolumn = '0' -- '0' is not bad
+  --       vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+  --       vim.o.foldlevelstart = 99
+  --       vim.o.foldenable = true
+  --   end,
+  --   config = function()
+  --     require'ufo'.setup{
+  --       provider_selector = function(bufnr, filetype, buftype)
+  --         return {'treesitter', 'indent'}
+  --       end
+  --     }
+  --   end
+  -- }
 }
