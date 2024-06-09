b8f6328279f8bbaeee590b98b2e056e2dd81beed
Daniel Owens <kyoji1@gmail.com>
2024-06-08 22:11:32 -0400

    Add a plugin to handle orgmode

---
+++ lua/plugins/org/init.lua
@@ -0,0 +1,17 @@
+return {
+  -- {
+  --   'nvim-orgmode/orgmode',
+  --   event = 'VeryLazy',
+  --   ft = { 'org' },
+  --   config = function()
+  --     -- Setup orgmode
+  --     require('orgmode').setup({
+  --       org_agenda_files = '~/orgfiles/**/*',
+  --       org_default_notes_file = '~/orgfiles/refile.org',
+  --     })
+  --   end,
+  -- }
+  {
+    'tentaclius/my-vim-org'
+  }
+}
