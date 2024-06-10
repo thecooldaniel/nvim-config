b210caa110fb531f9ff010d2e69067be65a28bca
Daniel Owens <kyoji1@gmail.com>
2024-06-09 23:29:31 -0400

    Change tabby theme

--- lua/plugins/navigation/init.lua
+++ lua/plugins/navigation/init.lua
@@ -89,38 +89,55 @@ return {
     -- event = 'VimEnter', -- if you want lazy load, see below
     dependencies = 'nvim-tree/nvim-web-devicons',
     config = function()
-
       local theme = {
-        current = { fg = "#cad3f5", bg = "transparent", style = "bold" },
-        not_current = { fg = "#5b6078", bg = "transparent" },
-
-        fill = { bg = "transparent" },
+        fill = 'TabLineFill',
+        -- Also you can do this: fill = { fg='#f2e9de', bg='#907aa9', style='italic' }
+        head = 'TabLine',
+        current_tab = 'TabLineSel',
+        tab = 'TabLine',
+        win = 'TabLine',
+        tail = 'TabLine',
       }
-      require("tabby.tabline").set(function(line)
-        return {
-          line.tabs().foreach(function(tab)
-            local hl = tab.is_current() and theme.current or theme.not_current
-            return {
-              line.sep(" ", hl, theme.fill),
-              tab.name(),
-              line.sep(" ", hl, theme.fill),
-              hl = hl,
-            }
-          end),
-          line.spacer(),
-          line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
-            local hl = win.is_current() and theme.current or theme.not_current
-
-            return {
-              line.sep(" ", hl, theme.fill),
-              win.buf_name(),
-              line.sep(" ", hl, theme.fill),
-              hl = hl,
-            }
-          end),
-          hl = theme.fill,
-        }
-      end)
+      require('tabby').setup({
+        line = function(line)
+          return {
+            {
+              { '  ', hl = theme.head },
+              line.sep(' ', theme.head, theme.fill),
+            },
+            line.tabs().foreach(function(tab)
+              local hl = tab.is_current() and theme.current_tab or theme.tab
+              return {
+                line.sep(' ', hl, theme.fill),
+                tab.is_current() and '' or ' ',
+                tab.number(),
+                tab.name(),
+                tab.close_btn(''),
+                line.sep(' ', hl, theme.fill),
+                hl = hl,
+                margin = ' ',
+              }
+            end),
+            line.spacer(),
+            line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
+              return {
+                line.sep(' ', theme.win, theme.fill),
+                win.is_current() and '' or ' ',
+                win.buf_name(),
+                line.sep(' ', theme.win, theme.fill),
+                hl = theme.win,
+                margin = ' ',
+              }
+            end),
+            {
+              line.sep(' ', theme.tail, theme.fill),
+              { '  ', hl = theme.tail },
+            },
+            hl = theme.fill,
+          }
+        end,
+        -- option = {}, -- setup modules' option,
+      })
     end,
   },
   {
