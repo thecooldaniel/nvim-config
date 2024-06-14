d515a845ae08737a7b3ad96ce55e3146f2985b79
Daniel Owens <kyoji1@gmail.com>
2024-06-13 21:41:57 -0400

    switch back to nightfly :)

--- lua/settings/after-plugins.lua
+++ lua/settings/after-plugins.lua
@@ -1,7 +1,7 @@
 -- Set colorscheme
 vim.o.background = "dark"
 local colorscheme_fallback = "habamax"
-local colorscheme = "rose-pine-main"
+local colorscheme = "nightfly"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
