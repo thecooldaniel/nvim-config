823b50c62fc0475422eded1c6e9b05a0baaa505d
daniel <12212+thecooldaniel@users.noreply.github.com>
2024-07-17 22:06:33 -0400

    Switch to 'Veridian' for SV LSP

--- lua/plugins/language/systemverilog.lua
+++ lua/plugins/language/systemverilog.lua
@@ -1,19 +1,30 @@
 local systemverilog = {}
 
 function systemverilog.setupLsp()
-  require('lspconfig').verible.setup({
-    -- cmd = { 'verible-verilog-ls', '--rules_config_search' },
-    cmd = { 'verible-verilog-ls' },
-    root_dir = require('lspconfig').util.root_pattern({'.git', 'verilator.f'}),
-    format_on_save = true,
-    capabilites = require'cmp_nvim_lsp'.default_capabilities(),
-  })
+  -- require('lspconfig').verible.setup({
+  --   -- cmd = { 'verible-verilog-ls', '--rules_config_search' },
+  --   cmd = { 'verible-verilog-ls' },
+  --   root_dir = require('lspconfig').util.root_pattern({'.git', 'verilator.f'}),
+  --   format_on_save = true,
+  --   capabilites = require'cmp_nvim_lsp'.default_capabilities(),
+  -- })
   -- return {
   --   cmd = { 'verible-verilog-ls' },
   --   root_dir = require('lspconfig').util.root_pattern({'.git', 'verilator.f'}),
   --   format_on_save = true,
   --   capabilites = require'cmp_nvim_lsp'.default_capabilities(),
   -- }
+  local lspconfutil = require 'lspconfig/util'
+  local root_pattern = lspconfutil.root_pattern("veridian.yml", ".git")
+  require('lspconfig').veridian.setup {
+    cmd = { 'veridian' },
+    root_dir = function(fname)
+      local filename = lspconfutil.path.is_absolute(fname) and fname
+      or lspconfutil.path.join(vim.loop.cwd(), fname)
+      return root_pattern(filename) or lspconfutil.path.dirname(filename)
+    end;
+    capabilites = require'cmp_nvim_lsp'.default_capabilities(),
+  }
 end
 
 function systemverilog.setupLinter(lint)
