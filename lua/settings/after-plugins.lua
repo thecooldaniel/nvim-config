bf97fc0042092c67d233cd92fb0d5de2a852b4ac
Daniel Owens <12212+thecooldaniel@users.noreply.github.com>
2024-08-12 10:21:06 -0400

    Chnage default colorscheme

--- lua/settings/after-plugins.lua
+++ lua/settings/after-plugins.lua
@@ -1,9 +1,23 @@
 -- Set colorscheme
 vim.o.background = "dark"
 local colorscheme_fallback = "habamax"
-local colorscheme = "cyberdream"
+local colorscheme = "nightfly"
+
+
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if status_ok then
+   if colorscheme == "cyberdream" then
+      local command = 'kitty @ set-background-image ' .. vim.fn.expand('/home/daniel/Dropbox/Wallpaper/Terminal/cyberdream_replica.png')
+      vim.fn.jobstart(command, {
+         on_stderr = function(_, d, _)
+            if #d > 1 then
+               vim.api.nvim_err_writeln("Could not change kitty background with command " .. command)
+            end
+         end
+      })
+   end
+end
 if not status_ok then
    vim.notify("colorscheme " .. colorscheme .. " not found, using fallback " .. colorscheme_fallback)
    vim.cmd 'colorscheme habamax'
